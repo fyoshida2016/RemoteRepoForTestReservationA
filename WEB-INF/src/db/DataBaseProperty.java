84249a15d005ff4e829b99e159d969fd90be5f3d
s163390 <s163390@ICS823.icsuser.nagaokaut.ac.jp>
2016-12-05 15:23:00 +0900

    DataBase完了

--- WEB-INF/src/db/DataBaseProperty.java
+++ WEB-INF/src/db/DataBaseProperty.java
@@ -3,9 +3,9 @@ package db;
 public class DataBaseProperty {
 	static final public String DB_URL = "jdbc:sqlserver://mis.nagaokaut.ac.jp";
 
-	static final public String DB_NAME = "SeTeamA";
+	static final public String DB_NAME = "163390";
 
-	static final public String DB_USERNAME = "SeTeamA";
+	static final public String DB_USERNAME = "163390";
 
-	static final public String DB_PASSWORD = "SeTeamA";
+	static final public String DB_PASSWORD = "16339082";
 }
